bc7b369ffbfea9f2610e612cbf4dfb4abdc476c5
Phil <pboileau.swim@gmail.com>
2017-06-13 12:37:28 -0400

    Finish simulating data

--- dummyData.R
+++ dummyData.R
@@ -16,7 +16,7 @@ library(igraph)
 # essentially the degree of the ego
 vertNum <- ceiling(rnorm(40, mean = 7, sd = 3))
 # make sure none are zero
-vertNum[which(vertNum == 0)] <- 1
+vertNum[which(vertNum <= 0)] <- 1
 
 # create a list of 40 ego-networks
 egoNets <- list()
@@ -39,37 +39,63 @@ for(j in 1:9){
 }
 
 # clean up
-rm(i, j)
-dev.off()
-
+rm(i, j, erdosI)
+#dev.off()
 
 
 # create a dataframe to store info on all the simulated values
-overview.df <- data.frame()
-
 # add bmi
 # generate the values for the BMI
 bmi <- rnorm(40, mean = 25, 5)
 bmiAve <- rnorm(40, mean = 25, 2)
-overview.df$bmi <- bmi
+overview.df <- as.data.frame(bmi)
 overview.df$bmiAve <- bmiAve
 rm(bmi, bmiAve)
 
 # add ego degree
 overview.df$egoDeg <- vertNum
 
+# add gender (0 is girls, 1 is boys)
+overview.df$gender <- round(runif(40))
+
 # calculate the values of (insert here) for each network
 overview.df$homo <- runif(40)
 overview.df$krackhardt <- runif(40, min = -1, max = 1)
 overview.df$yulesq <- runif(40, min = -1, max = 1)
 overview.df$IQV <- runif(40)
+overview.df$constrain <- runif(40)
+overview.df$hierarchy <- runif(40)
+
+# further calculations of the networks structural properties
+edgeDensity <- c()
+components <- c()
+edgeCount <- c()
+effectiveSize <- c()
+efficiency <- c()
+
+for(i in 1:40){
+  
+  edgeDensity <- c(edgeDensity, edge_density(egoNets[[i]], loops = FALSE))
+  components <- c(components, count_components(egoNets[[i]]))
+  edgeCount <- c(edgeCount, gsize(egoNets[[i]]))
+  effectiveSize <- c(effectiveSize, vcount(egoNets[[i]]) - sum(degree(egoNets[[i]], mode = "all"))/vertNum[i])
+  efficiency <- c(efficiency, effectiveSize[i] / vertNum[i])
+
+}
+
+# add values to the df
+overview.df$density <- edgeDensity
+overview.df$components <- components
+overview.df$edgeNum <- edgeCount
+overview.df$effectiveSize <- effectiveSize
+overview.df$efficiency <- efficiency
+
+rm(i, edgeDensity, vertNum, components, edgeCount, effectiveSize, efficiency)
+par(mfrow=c(1, 1))
 
 
 
-
-
-
-
+# time to create the first heatmap. This will just give an overview of the various 
 
 
 
