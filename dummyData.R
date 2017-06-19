fd2d9b7bd1a4165afd5754c0d8ea0a3bf4adbdac
Phil <pboileau.swim@gmail.com>
2017-06-19 15:42:19 -0400

    Revert "Correlated simulated data in data simulator"
    This reverts commit 095723604001c72f5795ea2aa0342cd1fabca405.

--- dummyData.R
+++ dummyData.R
@@ -121,7 +121,7 @@ d3heatmap(overview.df, scale = "column", dendrogram = "none", colors = "YlOrRd",
 
 library(heatmaply)
 heatmaply(percentize(overview.df),
-          dendrogram = "row",
+          dendrogram = "none",
           main = "Overview of Egonets",
           xlab = "Characteristics",
           ylab = "Egonets",
