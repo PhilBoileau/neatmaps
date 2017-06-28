da1346393b395aaf292866881dee352b8ad99b5d
Phil <pboileau.swim@gmail.com>
2017-06-28 16:17:46 -0400

    Skeleton for egonet data frame creator

---
+++ R/egonetDataFrame.R
@@ -0,0 +1,103 @@
+#' @title Ego Network Data Frames
+#' 
+#' @description 
+#' \code{egonet.data.frame} produces data frames of collections of ego networks.
+#' 
+#' @details
+#' The function produces data frames of collections of ego networks. The function
+#' requires the input of three data frames: a data frame containing the graph
+#' attributes, a data frame containing the alter characteristics and a data frame
+#' containing the edgelist of each network. The rows in each of these data frames
+#' must represent individual ego networks, and must therefore have identical row
+#' length. Measures of centrality used in the summarization of the alter attributes
+#' must also be furnished. If users wish to analyze the homophily of alters in the 
+#' ego networks, the variables to be analyzed must be indicated along with the
+#' measures of homophily to be used. All of the ego networks should be of similar
+#' size.
+#' 
+#' @param net.attr.df A data frame consisting of all of the ego networks' graph
+#'   attributes. The first column should contain the name of the network, and all
+#'   other columns should be numeric. All empty entries should be filled as "NA".
+#' @param alter.attr.df A data frame consisting of all of the ego networks' alters'
+#'   attributes. All columns should be numeric. All empty entries should be filled
+#'   in as "NA".
+#' @param edge.df A dataframe consisting of the edge matrix for each ego network.
+#'   Edges are assumed to be undirected and unweighted. 1 indicates the existence 
+#'   of an edge between alters, 0 indicates the lack of an edge.
+#' @param measuresOfCent A vector of the measures of centrality to be used for the
+#'   summary of the alter attributes data. The supported measures of centrality
+#'   are: "mean", "mode" and "median". Defaults to "mean".
+#' @param homoVars A vector consisting of the name of the variables on which homophily
+#'   is to be measured. These variables are dropped from the final data frame since 
+#'   they are catigorical variables.
+#' @param homoMeasures A vector consisting of the various methods to calculate homophily
+#'   with. The supported measures are: "homophily", "EIindex" and "IQV".
+#' @param scale A string indicating whether the data frame should be scaled, and if so,
+#'   which method should be used. The options are "none", "normalize" and "percentize".
+#'   If "none" is selected, then no scaling takes place. If "normalize" is selected,
+#'   each column is normalized to have a mean of 0 and standard deviation of 1. If 
+#'   "percentize" is selected, column values are transformed into percentiles. This
+#'   conserves the underlying distribution of the data if the data is not expected to
+#'   be normally distributed. Defaults to "none".
+#'   
+#' @return The function returns a data frame that offers an overview of all of the
+#'   ego networks.
+#'   
+#' @examples
+#' 
+egonet.data.frame <- function(net.attr.df, alter.attr.df, edge.df, measuresOfCent = c("mean"),
+                              homoVars = NULL, homoMeasures = NULL, scale.df = "none"){
+
+  # Make sure that only data frames are being passed in to the three first arguments
+  if(class(net.attr.df) != "data.frame" || class(alter.attr.df) != "data.frame" ||
+     class(edge.df) != "data.frame" || length(net.attr.df) != length(alter.attr.df) ||
+     length(net.attr.df) != length(edge.df))
+    stop("Please make sure tha net.attr.df, alter.attr.df and edge.df 
+         are dataframes of the same length.")
+  
+  
+  # define the data frame to be returned, set initially to net.attr.df
+  df <- net.attr.df
+  
+  
+  # create the igraph network list of all the egonets, only including variables used
+  # to measure homophily
+  # egonetList <- createNetworks(alter.attr.df, edge.df, homoMeasures)
+  
+  # get the structural characteristics of each of egonetworks
+  # structure.df <- getStructureAttr(egonetList, homoMeasures)
+  
+  
+  # drop the homophily measure variables from the alter.attr.df
+  alter.attr.df <- alter.attr.df[, !(names(alter.attr.df) %in% homoVars)]
+  
+  # get the measures of centrality of the alter characteristics
+  # alter.measures.df <- compactAlterAttr(alter.attr.df, measuresOfCent)
+  
+  
+  # combine all three data frames to get the final df
+  # df <- cbind(df, structure.df, alter.measures.df)
+  
+  
+  # scale the data frame, if requested
+  if(scale.df == "normalize")
+    df <- scale(df)
+  else if(scale.df == "percentize")
+    df <- percentize(df)
+  
+  
+  # return the final product
+  return(df)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
