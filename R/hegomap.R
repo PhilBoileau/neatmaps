da72a94901adf6aece193331f1a6e2562d21bfe9
Phil <pboileau.swim@gmail.com>
2017-07-13 14:25:24 -0400

    Finished hegomaps function

--- R/hegomap.R
+++ R/hegomap.R
@@ -14,7 +14,7 @@
 #'   numeric values
 #' @param printCluster a boolean value to determine if the cluster analysis
 #'   should be printed to the console.
-#' @param scale A string indicating whether the data frame should be scaled, and if so,
+#' @param scale.df A string indicating whether the data frame should be scaled, and if so,
 #'   which method should be used. The options are "none", "basic", "normalize" and "percentize".
 #'   If "none" is selected, then no scaling takes place.  If "basic" is selected, each
 #'   column is reduced to the 0-1 interval, whithout changing the underlying distribution. 
@@ -28,26 +28,45 @@
 #' @author Phil Boileau <philippe.boileau@mail.concordia.ca>
 #' @example
 #' 
-hegomaps <- function(df, printCluster = FALSE){
+hegomaps <- function(df, scale.df, method.hclust = "average", method.dist = "euclidean",
+                     nBootRep = 1000, cexLables = 1, cexAU = 1, pAlpha = 0.95,
+                     mainTitle, xlabel, ylabel, xlabFontSize = 5, ylabFontSize = 5,
+                     heatmapMargins = c(50, 50, 50, 100), printCluster = FALSE){
   
   # check dataframe to make sure that it only contains numeric values
   if(FALSE %in% sapply(df, is.numeric))
     stop("Please only input dataframes that contain numeric values")
+  
+  # scale the data based on user selection
+  if(scale.df == "basic")
+    df <- scaleColumns(df)
+  else if(scale.df == "normalize")
+    df <- scale(df)
+  else if(scale.df == "percentize")
+    df <- percentize(df)
+  
+  
+  # perform the cluster analysis on the variables of the networks
+  results <- pvclust(df, method.hclust = link.method, method.dist = dist.method,
+                     nboot = nBootRep)
+  dend <- as.dendrogram(results)
+  if(printCluster == TRUE){
+    labels_cex(dend) <- cexLabels
+    dend %>% pvclust_show_signif(results, show_type = "lwd", signif_type = "au") %>%
+      plot(main = 
+             paste("Cluster Dendrogram with AU/BP values (%)\n AU Values Highlighted by Signif -\n",
+                   dist.method, "/", link.method))
+    results %>% text(cex = cexAU)
+    results %>% pvrect(alpha = pAlpha)
+  }
+  
+  # create the heatmap
+  heatmaply(df, Colv = dend, main = mainTitle,
+            xlab = xlabel,
+            ylab = ylabel,
+            margins = heatmapMargins,
+            scale_fill_gradient_fun = ggplot2::scale_fill_gradient2(low = "blue", high = "red"),
+            cexRow = ylabFontSize,
+            cexCol = xlabFontSize)
+  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
