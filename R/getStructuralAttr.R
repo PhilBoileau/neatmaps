cf756f6759e2073221bdb927b21d242da51cbcc9
PhilBoileau <philippe_boileau@berkeley.edu>
2019-03-11 11:07:13 -0700

    Updated getStructuralAttr.R

--- R/getStructuralAttr.R
+++ R/getStructuralAttr.R
@@ -4,43 +4,47 @@
 #' \code{getStructureAttr} produces a data frame of the structural attributes
 #'   of a list of networks.
 #'   
-#' @param netList A list of Igraph network objects that represent the collection
+#' @param net_list A list of Igraph network objects that represent the collection
 #'   of networks.
 #'   
-#' @author Phil Boileau , \email{philippe.boileau@mail.concordia.ca}
+#' @author Philippe Boileau, \email{philippe_boileau@berkeley.edu}
 #' 
 #' @import igraph
-getStructureAttr <- function(netList){
+getStructureAttr <- function(net_list){
   
   # make sure that the argument is a list of igraph objects
-  if(class(netList) != "list" ||
-     sum(sapply(netList, class) == "igraph") != length(netList))
+  if(class(net_list) != "list" ||
+     sum(sapply(net_list, class) == "igraph") != length(net_list))
     stop("Please enter appropriate data as an argument.")
   
   # intialize the data frame that will contain all the information on
   # the structural characteristics of the data
-  listLen <- length(netList)
-  netID <- 1:listLen
-  df <- as.data.frame(netID)
+  list_len <- length(net_list)
+  net_id <- 1:list_len
+  df <- as.data.frame(net_id)
   
   # calculate the degree
-  df$netDegree <- sapply(1:listLen, function(x) vcount(netList[[x]]))
+  df$net_degree <- sapply(1:list_len, function(x) vcount(net_list[[x]]))
   # calculate the density
-  df$density <- sapply(1:listLen, function(x) graph.density(netList[[x]]))
+  df$density <- sapply(1:list_len, function(x) graph.density(net_list[[x]]))
   # count the number of components
-  df$components <- sapply(1:listLen, function(x) count_components(netList[[x]]))
+  df$components <- sapply(1:list_len, 
+                          function(x) count_components(net_list[[x]]))
   # calculate the number of edges
-  df$edgeCount <- sapply(1:listLen, function(x) ecount(netList[[x]]))
+  df$edge_count <- sapply(1:list_len, function(x) ecount(net_list[[x]]))
   # calculate the mean node degree
-  df$meanNodeDegree <- (df$edgeCount * 2) / df$netDegree
+  df$mean_node_deg <- (df$edge_count * 2) / df$net_degree
   # calculate the constraint of the graphs
-  df$constraint <- sapply(1:listLen, function(x) sum(constraint(netList[[x]]), na.rm = TRUE) /
-                            df$netDegree[x])
+  df$constraint <- sapply(1:list_len, 
+                          function(x){
+                            sum(constraint(net_list[[x]]), na.rm = TRUE) /
+                            df$net_degree[x]
+                          })
   # calculate the hierarchy of the graphs
-  df$hierarchy <- sapply(1:listLen, function(x) hierarchy(netList[[x]]))
+  df$hierarchy <- sapply(1:list_len, function(x) hierarchy(net_list[[x]]))
   
   # remove the ego ID
-  df$netID <- NULL
+  df$net_id <- NULL
   
   return(df)
   
