a8df0a51bf82cea3b75a8598526b152f9c3b8773
PhilBoileau <philippe_boileau@berkeley.edu>
2019-03-11 11:34:26 -0700

    Updated networkAttr.rda

---
+++ R/network_attr_df.R
@@ -0,0 +1,15 @@
+#' @name network_attr_df
+#' 
+#' @title Network Attributes Data
+#' 
+#' @description 
+#' A data set containing four randomly generated variables used to mimick the 
+#' network attributes of ten different networks. Attribute 1 and 2 have a 
+#' correlation of 0.41, 1 and 3 have a correlation of 0.91, 1 and 4 have a
+#' correlation of 0.34, 2 and 3 have a correlation of 0.07, 2 and 4 have a
+#' correlation of 0.17 and 3 and 4 have a correlation of 0.32.
+#' 
+#' @docType data
+#' @usage network_attr_df
+#' @author Philippe Boileau, \email{philippe_boileau@berkeley.edu}
+"network_attr_df" 
