55450551d1c5871a04030668c2a80ea38d85052a
PhilBoileau <philippe_boileau@berkeley.edu>
2019-03-11 11:45:10 -0700

    Updated scaledColumns.R

--- R/scaledColumns.R
+++ R/scaledColumns.R
@@ -1,17 +1,21 @@
 #' @title Scale Between 0 and 1
 #' 
 #' @description 
-#' \code{scaleColumns} scales the columns of a data frame object
-#'   between the values of 0 and 1 without changing the underlying
-#'   distribution of the columns.
+#' \code{scaleColumns} scales the columns of a data frame object between the
+#' values of 0 and 1 without changing the underlying distribution of the
+#' columns.
 #'   
-#' @author Phil Boileau , \email{philippe.boileau@mail.concordia.ca}
+#' @author Philippe Boileau , \email{philippe_boileau@berkeley.edu}
 #'  
 #' @param df The data frame of numerical values to be scaled.
 scaleColumns <- function(df){
   
-  scaledDf <- as.data.frame(
-    apply(df, 2, function(x) (x - min(x, na.rm = T))/(max(x, na.rm = T) - min(x, na.rm = T))))
+  scaled_df <- as.data.frame(
+    apply(df, 2,
+          function(x){
+            (x - min(x, na.rm = T))/(max(x, na.rm = T) - min(x, na.rm = T))
+    })
+  )
   
   return(scaledDf)
   
