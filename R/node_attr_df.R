712dcf39defdbddb6b5b6d3551ff1cc128327860
PhilBoileau <philippe_boileau@berkeley.edu>
2019-03-11 11:42:15 -0700

    Updated node attribute data

---
+++ R/node_attr_df.R
@@ -0,0 +1,15 @@
+#' @name node_attr_df
+#' 
+#' @title Node Attribute Data
+#' 
+#' @description 
+#' A dataset containing randomly generated node attributes for each node in each
+#' of the ten networks. Attributes A and B have a correlation of roughly 0.8, A
+#' and C have a correlation of roughly -0.2, B and C have a correlation of
+#' roughly 0.5. Attributes D and E were generated completely randomly, and
+#' should not be strongly correlated with any of the other attributes.
+#' 
+#' @docType data
+#' @usage node_attr_df
+#' @author Philippe Boileau, \email{philippe_boileau@berkeley.edu}
+"node_attr_df"
